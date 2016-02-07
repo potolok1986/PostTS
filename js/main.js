7f0cc95e45d3246ec43c4b838fe31bab220a7d16
Александр <potolok1986@gmail.com>
2016-02-07 19:35:35 +0600

    TS

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+/// <reference path="lib/declare/moment.d.ts"/>
+/// <reference path="lib/declare/jquery.d.ts"/>
+moment.locale("ru");
 var Config = (function () {
     function Config() {
         this.maxLength = 15;
-        this.minLength = 1;
+        this.minLength = 2;
         this.timeUpdate = 60000;
         this.storageName = "postsStorage";
         this.timeList = {
@@ -32,23 +35,141 @@ var MyLocalStorage = (function (_super) {
         this._storageName = this.storageName;
     }
     MyLocalStorage.prototype._updateStorage = function () {
-        localStorage.setItem(this._storageName, JSON.stringify(this._storage));
+        localStorage.setItem(this._storageName, JSON.stringify(this._storage.reverse()));
     };
-    MyLocalStorage.prototype.responseLocalStorage = function () {
-        this._storage = [];
+    MyLocalStorage.prototype.getStorage = function () {
         if (localStorage.getItem(this._storageName)) {
             this._storage = (JSON.parse(localStorage.getItem(this._storageName)));
+            return this._storage;
         }
-        return this._storage;
+        else {
+            return [];
+        }
     };
     MyLocalStorage.prototype.addToStorage = function (text) {
         var post = {
             text: text,
-            date: new moment()
+            date: moment()
         };
         this._storage.push(post);
         this._updateStorage();
     };
+    MyLocalStorage.prototype.delToStorage = function (index) {
+        this._storage.splice(index, 1);
+        this._updateStorage();
+    };
     return MyLocalStorage;
 })(Config);
+$(function () {
+    var Post = (function (_super) {
+        __extends(Post, _super);
+        function Post() {
+            _super.apply(this, arguments);
+            this._valid = false;
+            this.$elements = {
+                nothing: $("[data-post=noPost]"),
+                form: $("form"),
+                $postList: $(".postsList")
+            };
+        }
+        Post.prototype._dateTransform = function (date) {
+            var today = moment(), dateStorage = moment(date), dateString = "";
+            var result = today.diff(dateStorage, "minutes");
+            for (var time in this.timeList) {
+                if (result >= time) {
+                    this.timeList[time] === "max" ? dateString = moment(date).calendar() : dateString = this.timeList[time];
+                }
+            }
+            return dateString;
+        };
+        Post.prototype.getTextPost = function () {
+            return this._textPost;
+        };
+        Post.prototype.setTextPost = function (text) {
+            this._textPost = text;
+        };
+        Post.prototype.setValid = function (val) {
+            this._valid = val;
+        };
+        Post.prototype.getValid = function () {
+            return this._valid;
+        };
+        Post.prototype.displayStorage = function () {
+            this._result = this.getStorage();
+            if (this._result.length) {
+                this.showStorageList();
+            }
+            else {
+                this.$elements.nothing.removeClass("hidden");
+            }
+        };
+        Post.prototype.updateCounter = function (object, count) {
+            this.$elements.form.find("p:not(" + object + ")").hide().end()
+                .find(object).show()
+                .find("span").text(count);
+        };
+        Post.prototype.createPost = function (text, date) {
+            return $("<div/>", {
+                'class': "post",
+                html: [$("<p/>", {
+                        html: text
+                    }),
+                    $("<div/>", {
+                        'class': "time",
+                        html: [$("<p/>", {
+                                text: "Опубликованно&nbsp;",
+                                html: $("<span/>", {
+                                    text: this._dateTransform(date)
+                                })
+                            }),
+                            $("<button/>", {
+                                'class': "btn btn-danger",
+                                text: "Удалить"
+                            })]
+                    })]
+            });
+        };
+        Post.prototype.showStorageList = function () {
+            var postList = [], result = this._result;
+            for (var postId in result.reverse()) {
+                postList.push(this.createPost(result[postId].text, result[postId].date));
+            }
+            this.$elements.$postList.append(postList);
+        };
+        return Post;
+    })(MyLocalStorage);
+    var post = new Post;
+    post.displayStorage();
+    post.$elements.form
+        .on("submit", function (e) {
+        e.preventDefault();
+        if (post.getValid()) {
+            post.addToStorage(post.getTextPost());
+            post.$elements.$postList.prepend(post.createPost(post.getTextPost(), moment().format()));
+            $(e.target).find('textarea').val("");
+        }
+    })
+        .on("input", function (e) {
+        var value = e.target.value;
+        if (value.length < post.minLength) {
+            post.setValid(false);
+            post.updateCounter(".js-maxLength", post.minLength - value.length);
+        }
+        else if (value.length > post.maxLength) {
+            post.setValid(false);
+            post.updateCounter(".error", value.length - post.maxLength);
+        }
+        else {
+            post.setValid(true);
+            post.setTextPost(value);
+            post.updateCounter(".js-minLength", post.maxLength - value.length);
+        }
+    });
+    post.$elements.$postList.on("click", "button", function (e) {
+        e.preventDefault();
+        var $post = $(e.target).closest(".post");
+        post.delToStorage($post.index() - 1);
+        $post.remove();
+    });
+});
 //# sourceMappingURL=main.js.map
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,9 @@
+/// <reference path="lib/declare/moment.d.ts"/>
+/// <reference path="lib/declare/jquery.d.ts"/>
+moment.locale("ru")
 class Config {
     maxLength:number = 15;
-    minLength:number = 1;
+    minLength:number = 2;
     timeUpdate:number = 60000;
     storageName:string = "postsStorage";
     timeList = {
@@ -16,29 +19,155 @@ class Config {
         120: "max"
     };
 }
-interface NewPost{
+interface NewPost {
     text:string;
     date:Object;
 }
-class MyLocalStorage extends Config{
+class MyLocalStorage extends Config {
     private _storage = [];
-    private _storageName= this.storageName;
-    private _updateStorage(): void{
-        localStorage.setItem(this._storageName, JSON.stringify(this._storage));
+    private _storageName = this.storageName;
+
+    private _updateStorage() {
+        localStorage.setItem(this._storageName, JSON.stringify(this._storage.reverse()));
     }
-    public responseLocalStorage() {
-        this._storage = [];
-        if(localStorage.getItem(this._storageName)){
+
+    public getStorage() {
+        if (localStorage.getItem(this._storageName)) {
             this._storage = (JSON.parse(localStorage.getItem(this._storageName)));
+            return this._storage;
+        } else {
+            return [];
         }
-        return this._storage;
     }
-    public addToStorage(text:string){
-        var post : NewPost ={
-            text:text,
-            date: new moment()
+
+    public addToStorage(text:string) {
+        var post:NewPost = {
+            text: text,
+            date: moment()
         };
         this._storage.push(post);
         this._updateStorage();
     }
+
+    public delToStorage(index) {
+        this._storage.splice(index, 1);
+        this._updateStorage();
+    }
 }
+$(()=> {
+    class Post extends MyLocalStorage {
+        private _result;
+        private _valid:boolean = false;
+        private _textPost:string;
+        private _dateTransform(date:string):string {
+            var today = moment(),
+                dateStorage = moment(date),
+                dateString = "";
+            var result = today.diff(dateStorage, "minutes");
+            for (var time in this.timeList) {
+                if (result >= time) {
+                    this.timeList[time] === "max" ? dateString = moment(date).calendar() : dateString = this.timeList[time]
+                }
+            }
+            return dateString;
+        }
+
+        public getTextPost() {
+            return this._textPost
+        }
+
+        public setTextPost(text) {
+            this._textPost = text
+        }
+
+        public setValid(val:boolean) {
+            this._valid = val
+        }
+
+        public getValid() {
+            return this._valid
+        }
+
+        public $elements = {
+            nothing: $("[data-post=noPost]"),
+            form: $("form"),
+            $postList: $(".postsList")
+        };
+
+        public displayStorage() {
+            this._result = this.getStorage();
+            if (this._result.length) {
+                this.showStorageList();
+            } else {
+                this.$elements.nothing.removeClass("hidden");
+            }
+        }
+
+        public updateCounter(object:string, count:number) {
+            this.$elements.form.find("p:not(" + object + ")").hide().end()
+                .find(object).show()
+                .find("span").text(count);
+        }
+
+        public createPost(text:string, date:string) {
+            return $("<div/>", {
+                'class': "post",
+                html: [$("<p/>", {
+                    html: text
+                }),
+                    $("<div/>", {
+                        'class': "time",
+                        html: [$("<p/>", {
+                            text: "Опубликованно&nbsp;",
+                            html: $("<span/>", {
+                                text: this._dateTransform(date)
+                            })
+                        }),
+                            $("<button/>", {
+                                'class': "btn btn-danger",
+                                text: "Удалить"
+                            })]
+                    })]
+            })
+        }
+        public showStorageList() {
+            var postList = [],
+                result = this._result;
+            for (var postId in result.reverse()) {
+                postList.push(this.createPost(result[postId].text, result[postId].date))
+            }
+            this.$elements.$postList.append(postList);
+        }
+    }
+    var post:Post = new Post;
+    post.displayStorage();
+    post.$elements.form
+        .on("submit", (e) => {
+            e.preventDefault();
+            if (post.getValid()) {
+                post.addToStorage(post.getTextPost());
+                post.$elements.$postList.prepend(post.createPost(post.getTextPost(), moment().format()));
+                $(e.target).find('textarea').val("");
+            }
+        })
+        .on("input", (e) => {
+            var value:string = e.target.value;
+            if (value.length < post.minLength) {
+                post.setValid(false);
+                post.updateCounter(".js-maxLength", post.minLength - value.length);
+            } else if (value.length > post.maxLength) {
+                post.setValid(false);
+                post.updateCounter(".error", value.length - post.maxLength);
+            } else {
+                post.setValid(true);
+                post.setTextPost(value);
+                post.updateCounter(".js-minLength", post.maxLength - value.length);
+            }
+        });
+    post.$elements.$postList.on("click","button",(e)=>{
+        e.preventDefault();
+        var $post = $(e.target).closest(".post");
+        post.delToStorage($post.index() - 1);
+        $post.remove();
+    })
+});
